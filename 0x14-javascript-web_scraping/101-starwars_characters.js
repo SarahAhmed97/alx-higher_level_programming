f8d9c3163e402bc18f36074dbdb45dc8ccb51f5c
Sarah <sarah.ahmed9765@gmail.com>
2024-04-23 11:38:19 -0700

    task

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,25 +1,22 @@
 #!/usr/bin/node
-// Print all characters of a Star Wars movie
+// Prints all characters of a Star Wars movie
 const request = require('request');
-const url = 'https://swapi.co/api/films';
-request(url, { json: true }, (err, resp, body) => {
-  if (err) {
-    console.log(err);
-  } else if (body) {
-    for (const i in body.results) {
-      if (body.results[i].url.endsWith(`/${process.argv[2]}/`)) {
-        const characters = body.results[i].characters;
-        (func, ...args) { 
-        for (const j in characters) {
-          request(characters[j], { json: true }, (err, resp, body) => {
-            if (err) {
-              console.log(err);
-            } else if (body) {
-              console.log(body.name);
-            }
-          });
-        }
-      }
-    }
+request(`https://swapi-api.hbtn.io/api/films/${process.argv[2]}`, (error, response, body) => {
+  if (error) throw error;
+  else {
+    send(JSON.parse(body).characters, 0);
   }
 });
+
+function send (person, idx) {
+  if (idx >= person.length) {
+    return;
+  }
+  request(person[idx], (error, response, body) => {
+    if (error) throw error;
+    else {
+      console.log(JSON.parse(body).name);
+      return send(person, ++idx);
+    }
+  });
+}
