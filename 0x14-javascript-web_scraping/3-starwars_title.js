5d4ef855dbd202ba0b40bfdc04a72671b054ad5d
Sarah <sarah.ahmed9765@gmail.com>
2024-04-23 11:36:35 -0700

    task

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
-// Print the title of the Star Wars movie matching the given number
 const request = require('request');
-const url = `https://swapi.co/api/films/${process.argv[2]}`;
-request(url, { json: true }, (err, resp, body) => {
-  if (err) {
-    console.log(err);
-  } else if (body) {
-    console.log(body.title);
+const end = process.argv[2];
+
+request('https://swapi-api.hbtn.io/api/films/' + end, function (error, respones, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log(JSON.parse(body).title);
   }
 });
